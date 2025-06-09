f34bfcbc7e3dbb0c6657513717c68fe816de9e20
coder-002 <marcosprajapati@gmail.com>
2025-06-09 11:57:08 +0545

    create database and connected database

---
+++ src/main/java/org/ticket/event_ticket_management/EventticketmanagementApplication.java
@@ -0,0 +1,13 @@
+package org.ticket.event_ticket_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventticketmanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventticketmanagementApplication.class, args);
+    }
+
+}
