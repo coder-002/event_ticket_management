5b8c5a8dfb46e63010cf52fed4ca88f595663721
coder-002 <marcosprajapati@gmail.com>
2025-06-10 19:42:48 +0545

    create event response data

---
+++ src/main/java/org/ticket/event_ticket_management/domain/dtos/CreateEventRequestDto.java
@@ -0,0 +1,37 @@
+package org.ticket.event_ticket_management.domain.dtos;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.ticket.event_ticket_management.domain.entities.EventStatusEnum;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateEventRequestDto {
+
+    @NotBlank(message = "Event name is required")
+    private String name;
+    private LocalDateTime start;
+    private LocalDateTime end;
+
+    @NotBlank(message = "Venue info is required")
+    private String venue;
+
+    private LocalDateTime salesStart;
+    private LocalDateTime salesEnd;
+
+    @NotNull(message = "Event status must be provided")
+    private EventStatusEnum status;
+
+    @NotEmpty(message = "At least one ticket type is required")
+    @Valid
+    private List<CreateEventRequestDto> ticketTypes;
+}
---
+++ src/main/java/org/ticket/event_ticket_management/domain/dtos/CreateEventResponseDto.java
@@ -0,0 +1,29 @@
+package org.ticket.event_ticket_management.domain.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.ticket.event_ticket_management.domain.entities.EventStatusEnum;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateEventResponseDto {
+    private UUID id;
+    private String name;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private String venue;
+    private  LocalDateTime salesStart;
+    private  LocalDateTime salesEnd;
+    private EventStatusEnum status;
+
+    private List<CreateTicketTypeRequestDto> ticketTypes;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}
