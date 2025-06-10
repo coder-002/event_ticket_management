ae2131ac2059edc32962b16d4d08087897f4c561
coder-002 <marcosprajapati@gmail.com>
2025-06-10 16:54:14 +0545

    created enums

---
+++ src/main/java/org/ticket/event_ticket_management/domain/TicketStatusEnum.java
@@ -0,0 +1,5 @@
+package org.ticket.event_ticket_management.domain;
+
+public enum TicketStatusEnum {
+    PURCHASED, CANCELLED
+}
---
+++ src/main/java/org/ticket/event_ticket_management/domain/TicketValidationStatusEnum.java
@@ -0,0 +1,5 @@
+package org.ticket.event_ticket_management.domain;
+
+public enum TicketValidationStatusEnum {
+    VALID, INVALID, EXPIRED
+}
